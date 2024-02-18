cf5c31c759b15d234f1b0d4fd69f7b165de12598
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2024-02-18 15:08:14 +0000

    commit init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import  Card  from "./components/Card.tsx";
+function App() {
+  
+
+  return (
+    <>
+      <Card />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.tsx
@@ -0,0 +1,27 @@
+import "./Card.css";
+
+
+function Card () {
+    return ( 
+        <>
+<div className="container-card">
+    <div className="img-name">
+        <img src="https://avatars.githubusercontent.com/u/134714036?v=4" />
+        <h1>Rodrigo Neto</h1>
+        <p>Software Developer</p>
+        <section>
+        <ul>
+            <li>Instagram</li>
+            <li>LinkedIn</li>
+            <li>GitHub</li>
+        </ul>
+        </section>
+    </div>
+    
+</div>
+
+        </>
+    );
+}
+
+export default Card

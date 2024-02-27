2409d989ca9f80724718ac3d34be0c9a0a7dfc44
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2024-02-27 20:13:10 +0000

    fix: projets

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import './App.css'
 import  Card  from "./components/Card.tsx";
 import About  from "./components/About.tsx";
 import Skills from "./components/Skills.tsx";
+import Projets from "./components/Projets.tsx";
+
+
 function App() {
   
 
   return (
     <>
-      <Card />
+      {<Card />}
 <br/>
      {/* <About />
   <br />*/}
       {/*<Skills />   */}  
+      <Projets />
     </>
   )
 }

a492942067ae6dbfee36b074c526fb5c441660c0
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2024-03-02 19:20:05 +0000

    style: component skills

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,10 @@ function App() {
 
   return (
     <>
-      {/*<Card />
+      <Card />
 <br/>
       <About />
-  <br />*/}
+  <br />
       <Skills />  
      {/* <Projets />*/}
     </>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,7 +3,20 @@ import "./Skills.css";
 function Skills (){
     return (
         <main>
-      
+      <h1 className="title-skills">Technical Skills</h1>
+<div className="container-skills">
+    <div className="front-skills">
+        <img className="img-skills" src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg" alt="js"/>
+        <img className="img-skills" src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/typescript-colored.svg" alt="ts"/>
+        <img className="img-skills" src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg" alt="react"/>
+        <img className="img-skills" src="https://th.bing.com/th/id/R.d10df9bf6947cf1549e38bf57e2bc987?rik=kCPdvKNB0ltLFA&riu=http%3a%2f%2fi0.wp.com%2fcodemanbd.com%2fwp-content%2fuploads%2f2023%2f03%2fTailwind_CSS_Logo.svg.png&ehk=2VdX%2fhLT72Md5sdYY%2fGpWRfp2opeoQxdxX%2bZk0xQIaE%3d&risl=&pid=ImgRaw&r=0" alt="tailwind"/>
+        <img className="img-skills" src="https://raw.githubusercontent.com/styled-components/brand/master/styled-components.png" alt="css in js"/>
+        <img className="img-skills" src="https://th.bing.com/th/id/R.c502658a509d27b53679b3ef73c0d82f?rik=dFP%2b9LyCq64MMg&pid=ImgRaw&r=0" alt="node"/>
+        <img className="img-skills" src="https://th.bing.com/th/id/R.d638bd34b425dec39792d4a81189eddf?rik=2aVUd2lZxFKkUA&pid=ImgRaw&r=0" alt="express"/>
+        <img className="img-skills" src="https://picx.zhimg.com/v2-0117aac88b141a49021570df72b70d67_720w.jpg?source=172ae18b" alt="postgreSQL"/>
+        
+    </div>
+</div>
 
 
         </main>

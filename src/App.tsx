2b89d96052921597fbf294ec9f36da6fbb645af4
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2024-03-06 15:47:30 +0000

    fear: create component Object, style: color and card in progress..

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,18 @@ import About  from "./components/About.tsx";
 import Skills from "./components/Skills.tsx";
 import Projets from "./components/Projets.tsx";
 import Footer from "./components/Footer.tsx";
-
+import Object from "./components/Object.tsx";
 function App() {
   
 
   return (
     <>
-   <Card />
-   <br />
+   {/*<Card />
       <About />
   <Projets />
   <Skills />
-  <br /> <br />
-<Footer />
+  <Footer />*/}
+  <Object/>
     </>
   )
 }
---
+++ src/components/Object.tsx
@@ -0,0 +1,14 @@
+import "./Object.css";
+
+function Object (){
+    return(
+<main>
+    <div className="container-obj">
+<h1 className="title-obj">Objetivos</h1>
+<p></p>
+</div>
+</main>
+    );
+}
+
+export default Object

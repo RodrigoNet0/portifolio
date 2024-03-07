23884b36df14e7bbf05927979b42cf62bfe69244
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2024-03-07 22:53:43 +0000

    style: container ok, fear: text objectiv

--- src/components/Object.tsx
+++ src/components/Object.tsx
@@ -5,7 +5,8 @@ function Object (){
 <main>
     <div className="container-obj">
 <h1 className="title-obj">Objetivos</h1>
-<p></p>
+<p className="about-obj">Como desenvolvedor, meu objetivo é continuar evoluindo minhas habilidades técnicas e de liderança para ser um recurso valioso para a equipe e a empresa. Busco aprender novas tecnologias, liderar projetos desafiadores e colaborar proativamente para impulsionar a inovação e resolver problemas complexos. Estou comprometido com o crescimento pessoal e em contribuir significativamente para o sucesso da empresa onde estou inserido 
+</p>
 </div>
 </main>
     );

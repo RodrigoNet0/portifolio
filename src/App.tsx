22a6fd747b47ca3448fde32d999cff5e40e7bb26
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2024-03-08 00:57:33 +0000

    style: reponsive object in progress..

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,8 @@ function App() {
 
   return (
     <>
-   {/*<Card />
+  {/* <Card />
+   <br /> <br />
       <About />
   <Projets />
   <Skills />
--- src/components/Object.tsx
+++ src/components/Object.tsx
@@ -8,6 +8,7 @@ function Object (){
 <p className="about-obj">Como desenvolvedor, meu objetivo é continuar evoluindo minhas habilidades técnicas e de liderança para ser um recurso valioso para a equipe e a empresa. Busco aprender novas tecnologias, liderar projetos desafiadores e colaborar proativamente para impulsionar a inovação e resolver problemas complexos. Estou comprometido com o crescimento pessoal e em contribuir significativamente para o sucesso da empresa onde estou inserido 
 </p>
 </div>
+
 </main>
     );
 }
